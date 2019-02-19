ae76a1808136ab08f4cf867d00d01a087f478449
Ubuntu <ubuntu@ip-172-31-25-235.us-east-2.compute.internal>
2019-02-19 21:06:50 +0000

    Updated server-connect.service so that default date values are for current data rather than sample date.

--- src/app/services/server-connect.service.ts
+++ src/app/services/server-connect.service.ts
@@ -21,20 +21,20 @@ export class ServerConnectService {
 
     // ***comment out the following code to test sample orders from sample date 2019 JAN 01...
 
-    // this.currYear = `${date.getFullYear()}`;
-    // let monthNum = date.getMonth();
-    // let monthConversions = [{'num': 0, 'month': 'JAN'},{'num': 1, 'month': 'FEB'},{'num': 2, 'month': 'MAR'},
-    //   {'num': 3, 'month': 'APR'},{'num': 4, 'month': 'MAY'},{'num': 5, 'month': 'JUN'},
-    //   {'num': 6, 'month': 'JUL'},{'num': 7, 'month': 'AUG'},{'num': 8, 'month': 'SEP'},
-    //   {'num': 9, 'month': 'OCT'},{'num': 10, 'month': 'NOV'},{'num': 11, 'month': 'DEC'}];
-    // for(let i=0; i<monthConversions.length; i++){
-    //   if(monthNum == monthConversions[i].num){
-    //     this.currMonth = monthConversions[i].month;
-    //   }
-    // };
-    // this.currDay = `${date.getDate()}`;
-    // console.log(`${this.currYear} ${this.currMonth} ${this.currDay}`);
-    
+    this.currYear = `${date.getFullYear()}`;
+    let monthNum = date.getMonth();
+    let monthConversions = [{'num': 0, 'month': 'JAN'},{'num': 1, 'month': 'FEB'},{'num': 2, 'month': 'MAR'},
+      {'num': 3, 'month': 'APR'},{'num': 4, 'month': 'MAY'},{'num': 5, 'month': 'JUN'},
+      {'num': 6, 'month': 'JUL'},{'num': 7, 'month': 'AUG'},{'num': 8, 'month': 'SEP'},
+      {'num': 9, 'month': 'OCT'},{'num': 10, 'month': 'NOV'},{'num': 11, 'month': 'DEC'}];
+    for(let i=0; i<monthConversions.length; i++){
+      if(monthNum == monthConversions[i].num){
+        this.currMonth = monthConversions[i].month;
+      }
+    };
+    this.currDay = `${date.getDate()}`;
+    console.log(`${this.currYear} ${this.currMonth} ${this.currDay}`);
+       
     // ***(end of code to comment out for sample order and date usage)
   }
 
